e11a21b0a48f5ee1196afd30c4ebcdc07b3f4695
Sean Bohan <pixelpicosean@gmail.com>
2017-05-12 15:51:20 +0800

    Do not keep window for macOS app while quite.

--- electron-main.js
+++ electron-main.js
@@ -9,35 +9,35 @@ const BrowserWindow = electron.BrowserWindow;
 var mainWindow;
 
 function createWindow () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        fullscreen: false,
-        fullscreenable: false,
-        height: /*[[DEFAULT_GAME_HEIGHT*/500/*DEFAULT_GAME_HEIGHT]]*/, // This is changed via the setupGameSize script, change DEFAULT_GAME_HEIGHT to MAX_GAME_HEIGHT if you'd rather use the full size instead of only the safe zone size.
-        maximizable: false,
-        resizeable: false,
-        show: false,
-        useContentSize: true,
-        width: /*[[DEFAULT_GAME_WIDTH*/800/*DEFAULT_GAME_WIDTH]]*/ // This is changed via the setupGameSize script, change DEFAULT_GAME_WIDTH to MAX_GAME_WIDTH if you'd rather use the full size instead of only the safe zone size.
-    });
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    fullscreen: false,
+    fullscreenable: false,
+    height: 480,
+    maximizable: false,
+    resizeable: false,
+    show: false,
+    useContentSize: true,
+    width: 320,
+  });
 
-    mainWindow.once('ready-to-show', function() {
-        mainWindow.show();
-    });
+  mainWindow.once('ready-to-show', function() {
+    mainWindow.show();
+  });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -47,19 +47,15 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  app.quit();
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
